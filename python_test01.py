060a3fce64ec690d25c00631464f9ec3da914c57
ocd-dave <141537581+ocd-dave@users.noreply.github.com>
2025-08-18 21:11:55 -0700

    Add files via upload

---
+++ python_test01.py
@@ -0,0 +1,13 @@
+# python_test01.py
+# dM 8/16/25
+
+print("Welcome to python_test01.py")
+
+num1 - 2
+num2 - 4
+sumOfNums - num1 + num2
+print("")
+print("The sum of " + str(num1) + " and " + str(num2) + " is: " + str(sumOfNums))
+print()
+print()
+print("end of python_test01.py")
