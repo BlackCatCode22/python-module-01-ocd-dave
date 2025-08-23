9e477abff06674961b60e8a79961acf98d6616ab
ocd-dave <141537581+ocd-dave@users.noreply.github.com>
2025-08-23 11:04:41 -0700

    Add files via upload

--- syntax_logic_semantic_errors.py
+++ syntax_logic_semantic_errors.py
@@ -1,46 +1,21 @@
-#    >>> primt 'Hello world!'
-#    File "<stdin>", line 1
-#        primt 'Hello world!'
-#              ^^^^^^^^^^^^^^
-#    SyntaxError: invalid syntax
-
-#    >>> primt ('Hello world')
-#    Traceback (most recent call last):
-#    File "<stdin>", line 1, in <module>
-#    NameError: name 'primt' is not defined. Did you mean: 'print'?
-
-#    >>> I hate you Python!
-#    File "<stdin>", line 1
-#        I hate you Python!
-#          ^^^^
-#    SyntaxError: invalid syntax
-#    >>> if you come out of there, I would teach you a lesson
-#    File "<stdin>", line 1
-#        if you come out of there, I would teach you a lesson
-#               ^^^^
-#    SyntaxError: invalid syntax
-#    >>>
-
-
-print 'Hello world!'
-File "<stdin>", line 1
 print('Hello world!')
-      ^^^^^^^^^^^^^^
-SyntaxError: invalid syntax
+#File "<stdin>", line 1
+'Hello world!'
+#^^^^^^^^^^^^^^
+#SyntaxError: invalid syntax
 
 print('Hello world')
-Traceback (most recent call last):
-  File "<stdin>", line 1, in <module>
-NameError: name 'primt' is not defined. Did you mean: 'print'?
+#Traceback (most recent call last):
+#File "<stdin>", line 1, in <module>
+#NameError: name 'primt' is not defined. Did you mean: 'print'?
 
->>> I hate you Python!
-File "<stdin>", line 1
-    I hate you Python!
-      ^^^^
-SyntaxError: invalid syntax
->>> if you come out of there, I would teach you a lesson
-File "<stdin>", line 1
-    if you come out of there, I would teach you a lesson
-           ^^^^
-SyntaxError: invalid syntax
->>>
+print('I hate you Python!')
+#File "<stdin>", line 1
+'I hate you Python!'
+#^^^^
+#SyntaxError: invalid syntax
+print('if you come out of there, I would teach you a lesson')
+#File "<stdin>", line 1
+'if you come out of there, I would teach you a lesson'
+#^^^^
+#SyntaxError: invalid syntax
