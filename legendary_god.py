d62805dc7f56312929ff820b76bb0c11a81bfb4c
ocd-dave <141537581+ocd-dave@users.noreply.github.com>
2025-08-24 10:40:15 -0700

    Update legendary_god.py

--- legendary_god.py
+++ legendary_god.py
@@ -4,8 +4,15 @@ print('We have been waiting for you for a long time')
 
 print('Our legend says you will be very tasty with mustard')
 
-print 'We will have a feast tonight unless you say
+print('We will have a feast tonight unless you say')
 
 print('We will have a feast tonight unless you say')
 
+#"C:\Users\David Marrash\AppData\Local\Microsoft\WindowsApps\python3.13.exe" C:\python_demos\legendary_god.py
+#You must be the legendary god that comes from the sky
+#We have been waiting for you for a long time
+#Our legend says you will be very tasty with mustard
+#We will have a feast tonight unless you say
+#We will have a feast tonight unless you say
 
+#Process finished with exit code 0
