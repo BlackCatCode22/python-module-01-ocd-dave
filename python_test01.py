23ee78a2804f24c0c84e303011769f4a1a9357a4
ocd-dave <141537581+ocd-dave@users.noreply.github.com>
2025-08-27 12:32:56 -0700

    Update python_test01.py

--- python_test01.py
+++ python_test01.py
@@ -3,11 +3,12 @@
 
 print("Welcome to python_test01.py")
 
-num1 - 2
-num2 - 4
-sumOfNums - num1 + num2
+num1 = 2
+num2 = 4
+sumOfNums = num1 + num2
 print("")
 print("The sum of " + str(num1) + " and " + str(num2) + " is: " + str(sumOfNums))
 print()
 print()
+
 print("end of python_test01.py")
